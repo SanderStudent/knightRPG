b7ae3399e488f2b1c7622187b610f40cec564403
Sander Veter <sander.veter@persgroep.net>
2022-11-20 22:42:39 +0100

    add startServer

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
+
+func handleConnection(net.Conn) error {
+	return nil
+}
+
+func startServer() error {
+	log.Println("Starting server")
+
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		return err
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("Error accepting connection", err)
+			continue
+		}
+		handleConnection(conn)
+		if err != nil {
+			log.Println("Error handling connection", err)
+			continue
+		}
+		//go func() {
+		//	if err := handleConnection(conn, eventChannel); err != nil {
+		//		log.Println("Error handling connection", err)
+		//		return
+		//	}
+		//}()
+	}
+}
 
 func main() {
-	fmt.Println("Hello world")
+	err := startServer()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
